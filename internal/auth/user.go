154b94b461d4e0bc6e190fcf126bc6a37482b23c
Denis Krivak <d.krivak@fasten.com>
2018-09-09 19:22:14 +0300

    Omit user id in http api.

--- internal/auth/user.go
+++ internal/auth/user.go
@@ -4,7 +4,7 @@ import "time"
 
 // User represents a user that used for authenticating.
 type User struct {
-	ID       uint   `json:"id" gorm:"column:id"`
+	ID       uint   `json:"-" gorm:"column:id"`
 	Email    string `json:"email" gorm:"column:email"`
 	Password string `json:"-" gorm:"column:password"`
 
